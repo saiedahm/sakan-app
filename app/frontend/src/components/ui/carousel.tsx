ea73389618610ba92e4ee7d1aa26d01be66e2606
saiedahm <daralfan78@gmail.com>
2026-09-23 14:27:41 +0200

    Update carousel.tsx

--- app/frontend/src/components/ui/carousel.tsx
+++ app/frontend/src/components/ui/carousel.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+ import * as React from 'react';
 import useEmblaCarousel, {
   type UseEmblaCarouselType,
 } from 'embla-carousel-react';
@@ -83,19 +83,6 @@ const Carousel = React.forwardRef<
       api?.scrollNext();
     }, [api]);
 
-    const handleKeyDown = React.useCallback(
-      (event: React.KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === 'ArrowLeft') {
-          event.preventDefault();
-          scrollPrev();
-        } else if (event.key === 'ArrowRight') {
-          event.preventDefault();
-          scrollNext();
-        }
-      },
-      [scrollPrev, scrollNext]
-    );
-
     React.useEffect(() => {
       if (!api || !setApi) {
         return;
@@ -122,7 +109,7 @@ const Carousel = React.forwardRef<
       <CarouselContext.Provider
         value={{
           carouselRef,
-          api: api,
+          api,
           opts,
           orientation:
             orientation || (opts?.axis === 'y' ? 'vertical' : 'horizontal'),
@@ -134,7 +121,15 @@ const Carousel = React.forwardRef<
       >
         <div
           ref={ref}
-          onKeyDownCapture={handleKeyDown}
+          onKeyDownCapture={(event) => {
+            if (event.key === 'ArrowLeft') {
+              event.preventDefault();
+              scrollPrev();
+            } else if (event.key === 'ArrowRight') {
+              event.preventDefault();
+              scrollNext();
+            }
+          }}
           className={cn('relative', className)}
           role="region"
           aria-roledescription="carousel"
@@ -204,7 +199,7 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        'absolute  h-8 w-8 rounded-full',
+        'absolute h-8 w-8 rounded-full',
         orientation === 'horizontal'
           ? '-left-12 top-1/2 -translate-y-1/2'
           : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
@@ -214,7 +209,7 @@ const CarouselPrevious = React.forwardRef<
       onClick={scrollPrev}
       {...props}
     >
-      <ArrowLeft className="h-4 w-4" />
+      <ArrowLeft />
       <span className="sr-only">Previous slide</span>
     </Button>
   );
@@ -243,7 +238,7 @@ const CarouselNext = React.forwardRef<
       onClick={scrollNext}
       {...props}
     >
-      <ArrowRight className="h-4 w-4" />
+      <ArrowRight />
       <span className="sr-only">Next slide</span>
     </Button>
   );
