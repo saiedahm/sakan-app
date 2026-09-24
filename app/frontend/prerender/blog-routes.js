23f8e60bcfbc9d1e979d811b073165294f5053b2
saiedahm <daralfan78@gmail.com>
2026-09-24 19:07:29 +0200

    Delete app directory

--- app/frontend/prerender/blog-routes.js
+++
@@ -1,9 +0,0 @@
- const fs = require("fs");
-const path = require("path");
-
-const root = path.resolve(__dirname, "..");
-const blogRoutes = require(path.join(root, "src", "blog-routes.tsx"));
-
-module.exports = {
-  blogRoutes,
-};
