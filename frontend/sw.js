5fa3785675f6aeaa194ae778120c636770ecb7a0
saiedahm <daralfan78@gmail.com>
2026-09-26 08:13:46 +0200

    Create sw.js

---
+++ frontend/sw.js
@@ -0,0 +1,49 @@
+ const CACHE_NAME = "sakan-cache-v1";
+
+self.addEventListener("install", event => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request, {
+            cache: "no-store"
+        })
+        .then(response => {
+
+            if (response && response.ok) {
+
+                const copy = response.clone();
+
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, copy);
+                });
+            }
+
+            return response;
+        })
+        .catch(() => {
+            return caches.match(event.request);
+        })
+    );
+
+});
